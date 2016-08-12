e585409f663b1002e7a0a58431cc49223fcdfddc
Brian Rippeto <rippeto@gmail.com>
2016-08-11 18:47:32 -0600

    About to implement digit finding object, begin refactor

--- test/account-parser-test.js
+++ test/account-parser-test.js
@@ -4,27 +4,52 @@ const AccountParser = require('../lib/account-parser');
 // Scan a txt file line by line
 // Step one:  Identify all zeros after parsing txt file scenario
 describe('AccountParser.parse', () => {
-  var zeros=
+  var zeros =
   // 4 lines of 27 chars representing parsed txt file
   " _  _  _  _  _  _  _  _  _ " +
   "| || || || || || || || || |" +
   "|_||_||_||_||_||_||_||_||_|" +
   "                           ";
 
-  var accountParser = new AccountParser(zeros);
+  var ones =
+  "                           " +
+  "  |  |  |  |  |  |  |  |  |" +
+  "  |  |  |  |  |  |  |  |  |" +
+  "                           ";
+
+  var oneToNine =
+  "    _  _     _  _  _  _  _ " +
+  "  | _| _||_||_ |_   ||_||_|" +
+  "  ||_  _|  | _||_|  ||_| _|" +
+  "                           ";
+
+  var accountParseZeros = new AccountParser(zeros);
+  var accountParseOnes = new AccountParser(ones);
 
   it('can parse first digit', () => {
-    assert.equal(accountParser.digitOne(),
+    assert.equal(accountParseZeros.digitOne(),
     " _ " +
     "| |" +
     "|_|");
   })
 
   it('can identify first digit', () => {
-    assert.equal(accountParser.digitOneValue(), "0");
+    assert.equal(accountParseZeros.digitOneValue(), "0");
   })
 
   it('can parse all zeros', () => {
-    assert.equal(accountParser.accountNumber(), "000000000");
+    assert.equal(accountParseZeros.accountNumber(), "000000000");
+  })
+
+  it('can parse all ones', () => {
+    assert.equal(accountParseOnes.accountNumber(), "111111111");
+  })
+
+  // it('can parse all twos', () => {
+  //   assert.equal(accountParseTwos.accountNumber(), "222222222");
+  // })
+
+  xit('can parse one through nine', () => {
+    assert.equal(accountParseTwos.accountNumber(), "123456789");
   })
 });
